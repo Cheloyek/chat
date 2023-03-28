9ed2114d6261e298c2a71da1fd49ef278e4235d4
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-03-28 23:31:10 +0300

    add chat

--- chat_frontend/src/App.tsx
+++ chat_frontend/src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([
+      {message: "Good", id: "1", user: {id: "1", name: "userName1"} },
+    {message: "Good", id: "2", user: {id: "2", name: "userName2"} }
+  ])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chat
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            {messages.map(m => {
+               return  <div>
+                   <div style={{display: "flex", border: "1px solid blue", padding: "10px", height: "30px", width: "700px", alignItems: "center", overflowY: "scroll"}}><b>{m.user.name}:</b><span> {m.message}</span> <hr/></div>
+               </div>
+            })}
+        </div>
+        <ul id="messages"></ul>
+        <form id="form" action="">
+            <input id="input" autoComplete="off"/>
+            <button>Send</button>
+        </form>
     </div>
   );
 }
