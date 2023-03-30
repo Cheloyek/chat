c4b95313d353b2de21ba37b508c40b5db995c1c4
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-03-31 01:14:08 +0300

    fix: bug fixed

--- chat_frontend/src/App.tsx
+++ chat_frontend/src/App.tsx
@@ -2,10 +2,11 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client'
+import WebSock from "./WebSock";
 
 function App() {
     useEffect(() => {
-        let socket = io()
+        // let socket = io()
     }, [])
   const [messages, setMessages] = useState([
       {message: "Good", id: "1", user: {id: "1", name: "userName1"} },
@@ -15,20 +16,21 @@ function App() {
 
   return (
     <div className="App">
-        <div>
-        <div>
-            {messages.map(m => {
-               return  <div>
-                   <div style={{display: "flex", border: "1px solid blue", padding: "10px", height: "30px", width: "700px", alignItems: "center", overflowY: "scroll"}}><b>{m.user.name}:</b><span> {m.message}</span> <hr/></div>
-               </div>
-            })}
-        </div>
-        <ul id="messages"></ul>
-        <form id="form" action="">
-            <input id="input" autoComplete="off"/>
-            <button>Send</button>
-        </form>
-        </div>
+        <WebSock/>
+        {/*<div>*/}
+        {/*<div>*/}
+        {/*    {messages.map(m => {*/}
+        {/*       return  <div>*/}
+        {/*           <div style={{display: "flex", border: "1px solid blue", padding: "10px", height: "30px", width: "700px", alignItems: "center", overflowY: "scroll"}}><b>{m.user.name}:</b><span> {m.message}</span> <hr/></div>*/}
+        {/*       </div>*/}
+        {/*    })}*/}
+        {/*</div>*/}
+        {/*<ul id="messages"></ul>*/}
+        {/*<form id="form" action="">*/}
+        {/*    <input id="input" autoComplete="off"/>*/}
+        {/*    <button>Send</button>*/}
+        {/*</form>*/}
+        {/*</div>*/}
     </div>
   );
 }
---
+++ chat_frontend/src/WebSock.jsx
@@ -0,0 +1,109 @@
+import React, {useEffect, useRef, useState} from 'react';
+import axios from "axios";
+
+const WebSock = () => {
+    const [connected, setConnected] = useState(false)
+    const [userName, setUserName] = useState('')
+    const [messages, setMessages] = useState([])
+    const [value, setValue] = useState('')
+    const socket = useRef()
+
+    const connect = () => {
+        socket.current = new WebSocket('ws://localhost:3000')
+
+        socket.current.onopen = () => {
+            setConnected(true)
+            const message = {
+                event: 'connection',
+                userName,
+                id: Date.now()
+            }
+            //отправка сообщения
+            socket.current.send(JSON.stringify(message))
+            console.log('Подключение установлено')
+        }
+        socket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data)
+            setMessages(prev => [message, ...prev])
+            console.log('send message')
+
+        }
+        socket.current.onclose = () => {
+            console.log('close')
+
+        }
+        socket.current.onerror = () => {
+
+        }
+    }
+
+    useEffect(() => {
+
+    }, [])
+
+    const sendMessage = async () => {
+        const message = {
+            userName,
+            message: value,
+            id: Date.now(),
+            event: 'message'
+        }
+        socket.current.send(JSON.stringify(message))
+        setValue('')
+    }
+
+    if (!connected) {
+        return (
+            <div className="center">
+                <div className="form">
+                    <input type="text"
+                           placeholder={"Введите Ваше имя"}
+                           value={userName}
+                           onChange={e => setUserName(e.target.value)}/>
+                    <button onClick={connect}>Войти</button>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input value={value} onChange={e => setValue(e.target.value)} type="text"/>
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map(message => {
+                        return <div>
+                            <div key={message.id}
+                                 style={{
+                                     display: "flex",
+                                     border: "1px solid blue",
+                                     padding: "10px",
+                                     height: "30px",
+                                     width: "700px",
+                                     alignItems: "center",
+                                     overflowY: "scroll"
+                                 }}>
+                                {message.event === 'connection'
+                                ? <div>Пользователь {message.userName} подключился</div>
+                                : <div><b>{message.userName}:</b><span> {message.message}</span></div>
+                                }
+
+                                <hr/>
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </div>
+            {/*<ul id="messages"></ul>*/}
+            {/*<form id="form" action="">*/}
+            {/*    <input id="input" autoComplete="off"/>*/}
+            {/*    <button>Send</button>*/}
+            {/*</form>*/}
+        </div>
+    )
+
+};
+
+export default WebSock;
